e4bcfcd8311c2afdc732b6fea77bceaa313eb2f7
Alba Gómez Pérez <albagope@gmail.com>
2020-03-28 13:36:15 +0100

    create main html structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alba-gomez-starwars';
+  shipAtributte = '¿qué atributo soy?';
+  harry = [];
+
+  showAttribute() {
+    this.shipAtributte = 'soy otro';
+  }
+
+  showAttributeConsole() {
+    console.log('hola');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
