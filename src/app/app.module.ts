391159ac9b38af693d1fa06af6dc5149965f88e4
Alba Gómez Pérez <albagope@gmail.com>
2020-03-29 12:43:23 +0200

    import router and create new pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {HttpClientModule} from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import {DetailsComponent} from './details/details.component';
+import {HomeComponent} from './home/home.component';
 
-
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'ships', component: DetailsComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    DetailsComponent,
+    HomeComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
