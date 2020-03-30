92d258d28020cef4f0362538ca1574f08c10a234
Alba Gómez Pérez <albagope@gmail.com>
2020-03-30 12:03:49 +0200

    add data ships to html structure and create and move styles

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,7 @@ import {JsonService} from './json.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
 export class HomeComponent {
