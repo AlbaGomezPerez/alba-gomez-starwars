1b1e01c7b02f3e945f1f74045686fef74787f566
Alba Gómez Pérez <albagope@gmail.com>
2020-03-31 20:58:51 +0200

    Remove unused variable and create classes

--- src/app/list/json.service.ts
+++ src/app/list/json.service.ts
@@ -1,23 +1,45 @@
-import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class JsonService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   getJson(url: string, orderCriteria: string) {
-    const orderParam = orderCriteria;
-
-    return this.http.post(url, '{ "query": "query { allStarships(orderBy: ' + orderParam + ' ){ cargoCapacity costInCredits id name crew hyperdriveRating length maxAtmospheringSpeed passengers} } " }', {
+    return this.http.post<SearchResponse>(url, '{ "query": "query { allStarships(orderBy: ' + orderCriteria + ' ){ cargoCapacity costInCredits id name crew hyperdriveRating length maxAtmospheringSpeed passengers} } " }', {
       headers: {
         'Content-Type' : 'application/json',
         'Access-Control-Allow-Origin': '*',
       }
     });
   }
+}
+
+//TODO documentar
+export interface Starship {
+  id: string;
+  name: string;
+  crew: number;
+  cargoCapacity: number;
+  costInCredits: number;
+  hyperdriveRating: number;
+  length: number;
+  maxAtmospheringSpeed: number;
+  passengers: number;
+}
+
+//TODO documentar
+export interface StarshipsList {
+  allStarships: Array<Starship>;
+}
+
+//TODO documentar
+export interface SearchResponse {
+  data: StarshipsList;
 }
 
 
