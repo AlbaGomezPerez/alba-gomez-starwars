e27606e464f7bbd38bdcd12fb2ae454e9942661c
Alba Gómez Pérez <albagope@gmail.com>
2020-04-01 11:52:54 +0200

    create tests and variables

--- src/app/list/list.component.spec.ts
+++ src/app/list/list.component.spec.ts
@@ -1,26 +1,51 @@
-import { TestBed, async } from '@angular/core/testing';
+import {TestBed, async, ComponentFixture} from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ListComponent } from './list.component';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
-
+import {JsonService} from "./json.service";
 
 describe('ListComponent', () => {
+
+  let fixture: ComponentFixture<ListComponent>;
+  let compiled;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule
+        HttpClientTestingModule,
       ],
       declarations: [
         ListComponent
       ],
+      providers: [
+        JsonService
+      ]
     }).compileComponents();
+    fixture = TestBed.createComponent(ListComponent);
+    compiled = fixture.nativeElement;
   }));
 
-  it('should show buttons', () => {
-    const fixture = TestBed.createComponent(ListComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.btn-group-toggle div'));
+  it('should create the list', () => {
+    const list = fixture.componentInstance;
+    expect(list).toBeTruthy();
   });
+
+  it('should show buttons', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelectorAll('input[type=radio]').length).toEqual(7);
+  });
+
+  it('should show 37 progresss bars', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelectorAll('.progress div').length).toEqual(37);
+  });
+
+  it('first crew number should be 6', () => {
+    fixture.detectChanges();
+    expect(compiled.querySelector('.progress-name').textContent).toContain('Belbullab-22 starfighter');
+  });
+
 });
+//
+// document.querySelectorAll('.progress-name')[2]
