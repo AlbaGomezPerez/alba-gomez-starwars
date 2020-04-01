d8a0e5a279e3f9147a51f64e28510d08e4839983
Alba Gómez Pérez <albagope@gmail.com>
2020-04-01 12:40:46 +0200

    remove unused code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,11 +28,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Starships');
   });
-
-/*  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('alba-gomez-starwars app is running!');
-  });*/
 });
